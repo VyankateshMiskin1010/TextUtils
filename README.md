319e5351d396e2e6d1261ab4ac95b3c64fe7b43b
VyankateshMiskin1010 <121171811+VyankateshMiskin1010@users.noreply.github.com>
2023-09-22 12:20:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TextUtils
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-q3vehe)
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+  <StrictMode>
+    <App name="StackBlitz" />
+  </StrictMode>
+);
